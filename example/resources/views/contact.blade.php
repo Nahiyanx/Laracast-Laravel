7fab00400a9b5e34b8719143ade841a0ec7d9401
Nahiyanx <nahiyanx6@gmail.com>
2025-07-13 06:37:42 +0600

    Learned Upto Day 10
    Recently Learned Eloquent, Migrations and Factory.

---
+++ example/resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+
+    <x-slot:heading>
+
+        Contact Page
+
+    </x-slot:heading>
+
+    <h1>Welcome to the contact page!</h1>
+    
+</x-layout>
---
+++ example/resources/views/jobs.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+
+    <x-slot:heading>
+
+        Job Listings
+
+    </x-slot:heading>
+
+    @foreach ($jobs as $job)
+
+        <ul>
+
+
+    
+            <li>
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            
+                <strong> {{$job['title']}}: </strong> pays {{$job['salary']}} per year.
+                
+                </a>
+            </li>
+        
+
+        </ul>
+    @endforeach
+    
+</x-layout>
