5b2ac7b0418220b5f9ad5a885c7dbcca678627f9
Nahiyanx <nahiyanx6@gmail.com>
2025-07-17 13:25:22 +0600

    Followed Up-To Day 22
    - Made components for form button, form field, form error, form input, form label
    - Added a register page and login page under auth folder
    - Made a SessionController and RegisteredUserController for session control and user registration
    - Learned a lot about authentication

--- example/resources/views/Components/layout.blade.php
+++ example/resources/views/Components/layout.blade.php
@@ -19,7 +19,7 @@
   ```
 -->
 <div class="min-h-full">
-  <nav class="bg-gray-700">
+  <nav class="bg-gray-600">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
@@ -32,32 +32,25 @@
                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                 <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                 <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-              
             </div>
           </div>
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
-            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">View notifications</span>
-              <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-              </svg>
-            </button>
+            
+            @guest
+              <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+              <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+            @endguest
 
-            <!-- Profile dropdown -->
-            <div class="relative ml-3">
-              <div>
-                <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-hidden focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span class="absolute -inset-1.5"></span>
-                  <span class="sr-only">Open user menu</span>
-                  <img class="size-8 rounded-full" src="https://www.vhv.rs/dpng/d/232-2327111_transparent-recognition-clipart-face-recognition-logo-png-png.png" alt="" />
-                </button>
-              </div>
+            @auth
+              <form method="POST" action="/logout">
+                @csrf
+                <x-form-button>Log Out</x-form-button>
+              </form>
+            @endauth
 
-              
-            </div>
+
           </div>
         </div>
         <div class="-mr-2 flex md:hidden">
